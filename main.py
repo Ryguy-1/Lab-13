c33fdf917b668866093e7c742899d422e2a3e18b
Ryguy-1 <ryland.birchmeier.22@bishops.com>
2022-01-28 14:29:35 -0800

    first commit testing

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+def main():
+    data_frame = pd.read_csv('data/Lab13Export.csv', sep=',', header=0)
+    generate_graph(1, 2, data_frame)
+
+def generate_graph(index_1, index_2, data_frame):
+    # Get Headers
+    headers = data_frame.columns.values
+    plt.plot(data_frame[headers[index_1]], data_frame[headers[index_2]], 'ro')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Dependent Variable')
+    plt.title('Plot of 1 vs 2')
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    main()
