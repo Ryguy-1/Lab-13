09823ee2f0357d93f8c50ceddfcd65c66a73bdfc
Ryguy-1 <Ryguy-1@users.noreply.github.com>
2022-02-06 15:21:43 -0800

    updated graphs. run to get best fit lines equations

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import linregress
+# Import polyfit
+from scipy.optimize import curve_fit
 
 wheel_radius_cm = 5.1/2
 radius_axle_cm = 2.500/2
@@ -25,16 +27,49 @@ def plot_torque_vs_angular_acceleration():
     # Torque = Tension Force * Radius Axle
     torque_list = get_torque_list(tension_forces)
 
+    # Get Slope of Best Fit Line
     # Plot torque_list vs averaged_angular_acceleration_values
 
     for i in range(0, len(torque_list), 5):
         plt.plot(averaged_angular_acceleration_values[i:i+5], torque_list[i:i+5], 'ro')
+        # Plot Best Fit Line
+        slope, intercept, r_value, p_value, std_err = linregress(averaged_angular_acceleration_values[i:i+5], torque_list[i:i+5])
+        x = np.linspace(min(averaged_angular_acceleration_values[i:i+5]), max(averaged_angular_acceleration_values[i:i+5]), 100)
+        y = slope * x + intercept
+        plt.plot(x, y, 'b')
+        # Print equation of line
+        print('y = ' + str(slope) + 'x + ' + str(intercept), 'r^2 = ' + str(r_value**2))
         plt.xlabel('Angular Acceleration (rad/s^2)')
-        plt.ylabel('Torque (Nm)')
-        plt.title('Plot of Torque vs Angular Acceleration')
-        plt.savefig(f'data/experiment_{int(i/5+2)}_torque_vs_angular_acceleration.png')
+        plt.ylabel('Torque (Ncm)')
+        plt.title('Torque vs Angular Acceleration (Linear)')
+        plt.savefig(f'data/experiment_{int(i/5+2)}_torque_vs_angular_acceleration_linear.png')
         # Delete pyplot
         plt.clf()
+    
+    # For last two graphs, plot same thing, but with second degree polynomial fit
+    for i in range(10, len(torque_list), 5):
+        plt.plot(averaged_angular_acceleration_values[i:i+5], torque_list[i:i+5], 'ro')
+        # Plot polynomial fit
+        # Get polynomial coefficients
+        coeffs = np.polyfit(averaged_angular_acceleration_values[i:i+5], torque_list[i:i+5], 2)
+        # Get polynomial function
+        poly = np.poly1d(coeffs)
+        # Get x values
+        x = np.linspace(min(averaged_angular_acceleration_values[i:i+5]), max(averaged_angular_acceleration_values[i:i+5]), 100)
+        # Get y values
+        y = poly(x)
+        # Plot polynomial
+        plt.plot(x, y, 'b')
+        # Print equation of line
+        print('y = ' + str(coeffs[0]) + 'x^2 + ' + str(coeffs[1]) + 'x + ' + str(coeffs[2]), 'r^2 = ' + str(r_value**2))
+        plt.xlabel('Angular Acceleration (rad/s^2)')
+        plt.ylabel('Torque (Ncm)')
+        plt.title('Torque vs Angular Acceleration (Polynomial)')
+        plt.savefig(f'data/experiment_{int(i/5+2)}_torque_vs_angular_acceleration_polynomial.png')
+        # Delete pyplot
+        plt.clf()
+    
+    
 
 def get_torque_list(tension_forces):
     torque_list = []
@@ -57,20 +92,20 @@ def get_mass_accelerations(averaged_angular_acceleration_values):
     accelerations_list = []
     for acceleration in averaged_angular_acceleration_values:
         accelerations_list.append(acceleration * wheel_radius_cm)
-        print(accelerations_list[-1])
     return accelerations_list
+
+# Get Slope of Datapoints
+def get_slope(arr_x_y):
+    x = arr_x_y[0]
+    y = arr_x_y[1]
+    # Use Scipy to get slope
+    slope = linregress(x, y)[0]
+    return slope
 
 # Average Angular Accelerations
 def get_averaged_angular_acceleration_values(data_frame):
 
-    # Get Slope of Datapoints
-    def get_slope(arr_x_y):
-        x = arr_x_y[0]
-        y = arr_x_y[1]
-        # Use Scipy to get slope
-        slope = linregress(x, y)[0]
-        print(slope)
-        return slope
+
 
     # Get Position Data to take Slope of Line Through for Each Run
     position_data_per_run = [] # Format: [[time_values, velocity_values], [time_values, velocity_values], ...]
@@ -104,11 +139,11 @@ def get_averaged_angular_acceleration_values(data_frame):
     # Take Slope of Best Fit Line For Each Run and Append to Averaged Angular Acceleration List
     averaged_angular_acceleration_values = []
     for i in range(len(position_data_per_run)):
-        print(position_data_per_run[i])
         # Get Slope of Best Fit Line
         slope = get_slope(position_data_per_run[i])
         # Append to Averaged Angular Acceleration List
         averaged_angular_acceleration_values.append(slope)
+    print(averaged_angular_acceleration_values[2:])
 
     # for angular_acceleration_list in angular_acceleration_lists:
     #     averaged_angular_acceleration_values.append(np.mean(angular_acceleration_list))
